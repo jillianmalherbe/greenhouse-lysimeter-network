e7305fdba615a6a2bc497beb56de6f7f712338ea
Sydney <sydney.vieveen6412@gmail.com>
2025-05-26 11:34:16 -0400

    rpi5: drivers: load cell code + package requirements
    Signed-off-by: Sydney <sydney.vieveen6412@gmail.com>

---
+++ rpi5/drivers/loadCell.py
@@ -0,0 +1,84 @@
+"""
+######################################################################
+README:
+
+This version runs in python 3.x. It will first prompt the user to
+empty the scale. Then prompt user to place an item with a known weight
+on the scale and input weight as INT.
+
+The offset and scale will be adjusted accordingly and displayed for
+convenience.
+
+The user can choose to [0] exit, [1] recalibrate, or [2] display the
+current offset and scale values and weigh a new item to test the accuracy
+of the offset and scale values!
+#######################################################################
+"""
+
+import RPi.GPIO as GPIO
+import time
+import sys
+from hx711 import HX711
+
+class loadCell:
+    #Can print the mass and calibrate the load cell
+
+    # constructor for the load cell class
+    def __init__(self):
+        # Make sure you correct these to the correct pins for DOUT and SCK.
+        # gain is set to 128 as default, change as needed.
+        self.hx = HX711(15, 14, 128)
+        self.setup()
+
+    def setup(self):
+        # code run once to set up the sensor
+        print("Initializing.\n Please ensure that the scale is empty.")
+        scale_ready = False
+        while not scale_ready:
+            if (GPIO.input(self.hx.DOUT) == 0):
+                scale_ready = False
+            if (GPIO.input(self.hx.DOUT) == 1):
+                print("Initialization complete!")
+                scale_ready = True
+                #global logger
+                #logger = l.Logger()
+
+    def calibrate(self):
+        # calibrates the sensor with a known mass
+        readyCheck = input("Remove any items from scale. Press any key when ready.")
+        offset = self.hx.read_average()
+        print("Value at zero (offset): {}".format(offset))
+        self.hx.set_offset(offset)
+        print("Please place an item of known weight on the scale.")
+
+        readyCheck = input("Press any key to continue when ready.")
+        measured_weight = (self.hx.read_average()-self.hx.get_offset())
+        item_weight = input("Please enter the item's weight in grams.\n>")
+        scale = int(measured_weight)/int(item_weight)
+        self.hx.set_scale(scale)
+        print("Scale adjusted for grams: {}".format(scale))
+
+    # gets the offset of an item
+    def get_offset(self):
+        offset = self.hx.get_offset()
+        self.hx.power_down()
+        time.sleep(.001)
+        self.hx.power_up()
+        return offset
+
+    # gets the scale of an item
+    def get_scale(self):
+        scale = self.hx.get_scale()
+        self.hx.power_down()
+        time.sleep(.001)
+        self.hx.power_up()
+        return scale
+
+    # gets the mass of an item
+    def get_mass(self):
+        mass = self.hx.get_grams()
+        self.hx.power_down()
+        time.sleep(.001)
+        self.hx.power_up()
+        return mass
+
---
+++ rpi5/drivers/loadCell_test.py
@@ -0,0 +1,133 @@
+import CO2etal as co2
+import soilMoisture as moist
+import leafIR as ir
+import pressure as pres
+import loadCell as load
+import valve as v
+#import logger_csv as l
+import time
+from datetime import datetime
+
+# this stores the current time
+global curr_time
+curr_time=time.time()
+
+# calibrate the load cell
+load_cell = load.loadCell()
+load_cell.calibrate()
+
+# strores the logger information
+#logger = l.Logger()
+
+print("\nOffset: {}\nScale: {}".format(load_cell.get_offset(), load_cell.get_scale()))
+print("Item weighs {} grams.\n".format(load_cell.get_mass()))
+
+## check if 24 hours have elapsed then turn the valve on
+#def check_turn_on():
+#	global curr_time
+#	#if not(valve.is_on()) and curr_time-time.time()/3600 <= -24:
+#	if not(valve.is_on()) and curr_time-time.time() <= -30:
+#		curr_time=time.time()
+#		valve.turn_on()
+#
+#	# turns the valve off if it has been more than 15 minutes
+#	#if valve.is_on() and curr_time-time.time() <= -1800:
+#	if valve.is_on() and curr_time-time.time() <= -30:
+#		valve.turn_off()
+
+
+# loop through and print information in the csv file
+# create all sensors classes
+#try:
+#	co2_sensor = co2.CO2()
+#except:
+#	pass
+#
+#try:
+#	moist_sensor = moist.soilMoisture()
+#except:
+#	pass
+#try:
+#	pres_sensor = pres.Pressure()
+#except:
+#	pass
+#try:
+#	ir_sensor = ir.leafIR()
+#except:
+#	pass
+#try:
+#	valve = v.Valve()
+#except:
+#	pass
+
+# Run the loop function indefinitely to print the sensor information in the csv file
+while True:
+
+#	# get the c02 data
+#	try:
+#		print("Data available?", co2_sensor.get_data_available())
+#		co2_value=co2_sensor.get_co2()
+#		print("CO2:", co2_value, "PPM")
+#		co2_temp=co2_sensor.get_temp()
+#		print("Temperature:", co2_temp, "degrees C")
+#		co2_humid=co2_sensor.get_humid()
+#		print("Humidity:", co2_humid, "%%rH")
+#		'''logger.collect_data("Co2 (PPM)", co2_value)
+#		logger.collect_data("Co2_temp (°C)", co2_temp)
+#		logger.collect_data("Co2_humid (%%rH)", co2_humid)'''
+#	except:
+#		pass
+#
+#	try:
+#		# for the soil moisture sensor
+#		soil_temp=str(moist_sensor.get_temp())
+#		print("temp: " + soil_temp)
+#		soil_moist=str(moist_sensor.get_moist())
+#		print("moisture: " + soil_moist)
+#		'''logger.collect_data("Soil_temp (°C)", soil_temp)
+#		logger.collect_data("Soil_moist", soil_moist)'''
+#	except:
+#		pass
+#
+#	try:
+#		# Read data for pressure sensor
+#		bmp_temp, pressure = pres_sensor.get_temp_and_pressure()
+#		bmp_temp="{text:.2f}".format(text=bmp_temp)
+#		pressure="{text:.2f}".format(text=pressure)
+#
+#		# Print results
+#		print("Temperature: ", bmp_temp, "°C, Pressure: ",pressure," Pa")
+#		'''logger.collect_data("Bmp_temp (°C)", bmp_temp)
+#		logger.collect_data("Pressure (Pa)", pressure)'''
+#	except:
+#		pass
+#
+#	try:
+#		# Read data for leaf ir sensor
+#		ambient_temp = ir_sensor.get_ambient_temp()
+#		object_temp = ir_sensor.get_object_temp()
+#
+#		ambient_temp = "{text:.2f}".format(text=ambient_temp)
+#		object_temp = "{text:.2f}".format(text=object_temp)
+#
+#		print("Ambient Temperature: ",ambient_temp," °C")
+#		print("Object Temperature: ",object_temp," °C")
+#		'''logger.collect_data("Ambient_temp (°C)", ambient_temp)
+#		logger.collect_data("Object_temp (°C)", object_temp)'''
+#	except:
+#		pass
+
+	mass="{}".format(load_cell.get_mass())
+
+	print("Item weighs ",mass," grams.\n")
+
+	# add all the variables to the logger
+	'''logger.collect_data("Date", datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+	logger.collect_data("Mass", mass)
+	logger.log_data_csv()'''
+
+	time.sleep(5)
+	#logger.add_server()
+
+	# check if 24 hours have elapsed then turn the valve on (also turn off if needed)
+#	check_turn_on()
