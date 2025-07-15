42533b65c45d5c57d2b139027f19e0eba61d0d69
Jillian Malherbe <19jrm10@queensu.ca>
2025-07-15 15:42:30 -0400

    rpi5: drivers: scd41 and lps28
    Signed-off-by: Jillian Malherbe <19jrm10@queensu.ca>

---
+++ rpi5/drivers/main.py
@@ -0,0 +1,28 @@
+import time
+
+import board
+
+import adafruit_lps28
+import adafruit_scd4x
+
+i2c = board.I2C()
+
+lps28_sensor = adafruit_lps28.LPS28(i2c)
+scd41_sensor = adafruit_scd4x.SCD4X(i2c)
+print("Serial number:", [hex(i) for i in scd41_sensor.serial_number])
+
+scd41_sensor.start_periodic_measurement()
+
+print("Sensor Readings:")
+print("-" * 40)
+
+while True:
+    if lps28_sensor.data_ready:
+        print(f"LPS28 Pressure: {lps28_sensor.pressure:.1f} hPa")
+        print(f"LPS28 Temperature: {lps28_sensor.temperature:.1f} degrees Celcius")
+    if scd41_sensor.data_ready:
+        print("SCD41 CO2: %d ppm" % scd41_sensor.CO2)
+        print("SDC41 Temperature: %0.1f *C" % scd41_sensor.temperature)
+        print("SCD41 Humidity: %0.1f %%" % scd41_sensor.relative_humidity)
+        print()
+    time.sleep(5)
